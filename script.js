91ab6ab2033ca89e61e442dee14e6ad2df7513d3
Yasvanth Kumar <yasvanthkumar48@gmail.com>
2025-11-20 17:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ function insert_Row() {
   var cell1 = newRow.insertCell(0);
   var cell2 = newRow.insertCell(1);
 
-  cell1.innerHTML = 'New Cell1';
-  cell2.innerHTML = 'New Cell2';
+  cell1.textContent = 'New Cell1';
+  cell2.textContent = 'New Cell2';
 }
